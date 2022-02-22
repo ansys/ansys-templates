dba358d09d572c14b0d237288ecb617cf9afa075
Jorge Martínez <jorge.martinezgarrido@ansys.com>
2022-02-22 18:48:40 +0100

    Setup CI pipelines

---
+++ tests/test_template.py
@@ -0,0 +1,56 @@
+PRODUCT_NAME = "Product"
+PRODUCT_NAME_SLUG = PRODUCT_NAME.lower().replace(" ", "-").replace("_", "-")
+LIBRARY_NAME = "Library"
+LIBRARY_NAME_SLUG = LIBRARY_NAME.lower().replace(" ", "-").replace("_", "-")
+PROJECT_NAME_SLUG = f"py{PRODUCT_NAME_SLUG}-{LIBRARY_NAME_SLUG}"
+PKG_NAME = f"ansys-{PRODUCT_NAME_SLUG}-{LIBRARY_NAME_SLUG}"
+VERSION = "0.1.0"
+SHORT_DESCRIPTION = f"A Python wrapper for Ansys {PRODUCT_NAME} {LIBRARY_NAME}"
+REPOSITORY_URL = f"https://github.com/pyansys/{PROJECT_NAME_SLUG}"
+REQUIRES_PYTHON = "3.7"
+
+
+def test_bake_project(cookies):
+    result = cookies.bake(
+        extra_context={
+            "product_name": PRODUCT_NAME,
+            "product_name_slug": PRODUCT_NAME_SLUG,
+            "library_name": LIBRARY_NAME,
+            "library_name_slug": LIBRARY_NAME_SLUG,
+            "project_name_slug": PROJECT_NAME_SLUG,
+            "pkg_name": PKG_NAME,
+            "version": VERSION,
+            "short_description": SHORT_DESCRIPTION,
+            "repository_url": REPOSITORY_URL,
+            "requires_python": REQUIRES_PYTHON,
+        }
+    )
+
+    assert result.exit_code == 0, result.exception
+    assert result.exception is None
+    assert result.project_path.name == PROJECT_NAME_SLUG
+    assert result.project_path.is_dir()
+
+    files = [
+        ".github/workflows/ci_cd.yml",
+        ".gitignore",
+        "LICENSE",
+        "README.rst",
+        "pyproject.toml",
+        "tox.ini",
+    ]
+
+    dirs = [
+        ".github",
+        ".github/workflows",
+        "docs",
+        "src",
+        "src/ansys/" + PRODUCT_NAME_SLUG,
+        "src/ansys/" + PRODUCT_NAME_SLUG + "/" + LIBRARY_NAME_SLUG,
+        "tests",
+    ]
+
+    for path in files:
+        assert (result.project_path / path).is_file()
+    for path in dirs:
+        assert (result.project_path / path).is_dir()
