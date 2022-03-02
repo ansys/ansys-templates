89ea390ab56cf086df0f7ee088286b2d77df9d53
Jorge Martínez <jorge.martinezgarrido@ansys.com>
2022-03-02 11:26:20 +0100

    Apply baked project style

--- {{cookiecutter.project_name_slug}}/doc/source/conf.py
+++ {{cookiecutter.project_name_slug}}/doc/source/conf.py
@@ -13,18 +13,18 @@ release = version = __version__
 
 # use the default pyansys logo
 html_logo = pyansys_logo_black
-html_theme = 'pyansys_sphinx_theme'
+html_theme = "pyansys_sphinx_theme"
 
 # specify the location of your github repo
 html_theme_options = {
     "github_url": "{{ cookiecutter.repository_url }}",
-    "show_prev_next": False
+    "show_prev_next": False,
 }
 
 # Sphinx extensions
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
     "numpydoc",
     "sphinx.ext.intersphinx",
     "sphinx_copybutton",
@@ -33,9 +33,7 @@ extensions = [
 # Intersphinx mapping
 intersphinx_mapping = {
     "python": ("https://docs.python.org/dev", None),
-
     # kept here as an example
-
     # "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
     # "numpy": ("https://numpy.org/devdocs", None),
     # "matplotlib": ("https://matplotlib.org/stable", None),
@@ -73,7 +71,7 @@ html_static_path = ["_static"]
 templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
