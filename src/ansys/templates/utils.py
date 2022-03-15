6db5f4e8cb6a018654a1753574a6ddee68eadb27
Jorge Martinez <jorge.martinezgarrido@ansys.com>
2022-03-15 16:53:13 +0100

    Custom copytree function

--- src/ansys/templates/utils.py
+++ src/ansys/templates/utils.py
@@ -1,8 +1,99 @@
 """A collection of useful utilities and routines."""
-from cookiecutter.main import cookiecutter
+import os
+import stat
 import tempfile
 from pathlib import Path
-from shutil import copytree, copyfile, rmtree
+import shutil
+
+from cookiecutter.main import cookiecutter
+
+def _copytree(input_path, output_path):
+    """
+    Recursively copy all the contents of a directory into desired one.
+
+    Paremeters
+    ----------
+    input_path: ~pathlib.Path
+        Path of the source directory to be copied.
+    output_path: ~pathlib.Path
+        Path of the destination directory.
+
+    """
+
+    # Create output directory if it does not exist and ensure permission bits
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+        shutil.copystat(input_path, output_path)
+
+    # Get all file directories from the input directory
+    files_list = os.listdir(input_path)
+
+    for file in files_list:
+        # Create source and destintation file paths
+        source_path = os.path.join(input_path, file)
+        dest_path = os.path.join(output_path, file)
+
+        # Recursion is used in case a directory is found
+        if os.path.isdir(source_path):
+            copytree(source_path, dest_path)
+        else:
+          shutil.copy2(source_path, dest_path)
+
+
+def _copy_common_template_files(common_path, project_path):
+    """
+    Copy common template files into desired project directory.
+
+    Parameters
+    ----------
+    common_path: ~pathlib.Path
+        Path to the common template directory.
+    project_path: ~pathlib.Path
+        Path to the baked project directory.
+
+    """
+    copytree(
+        common_path / "{{cookiecutter.__project_name_slug}}", 
+        project_path / "{{cookiecutter.__project_name_slug}}", 
+    )
+
+
+def _copy_all_template_files(template_path, project_path):
+    """
+    Copy all template files including cookiecutter.json and hooks/ directory.
+
+    Parameters
+    ----------
+    template_path: ~pathlib.Path
+        Path to the template directory.
+    project_path: ~pathlib.Path
+        Path to the baked project directory.
+
+    """
+    copytree(
+        template_path, 
+        project_path, 
+    )
+
+
+def include_license(license_path, project_dirpath):
+    """
+    Include a desired license into the baked project.
+
+    Parameters
+    ----------
+    license_path: ~pathlib.Path
+        Path to the license template.
+    project_dirpath: ~pathlib.Path
+        Path to the baked project directory.
+
+    Notes
+    -----
+    This function is intended to be used during the pre_gen_project.py hook.
+
+    """
+    shutil.copyfile(license_path, project_dirpath + "/" + license_path.name)
+
 
 def bake_template(template_path, output_path, **cookiecutter_kwargs):
     """
@@ -41,56 +132,3 @@ def bake_template(template_path, output_path, **cookiecutter_kwargs):
             output_dir=str(output_path), 
             **cookiecutter_kwargs
         )
-
-
-def _copy_common_template_files(common_path, project_path):
-    """
-    Copy common template files into desired project directory.
-
-    Parameters
-    ----------
-    common_path: ~pathlib.Path
-        Path to the common template directory.
-    project_path: ~pathlib.Path
-        Path to the baked project directory.
-
-    """
-    copytree(
-        common_path / "{{cookiecutter.__project_name_slug}}", 
-        project_path / "{{cookiecutter.__project_name_slug}}", 
-        dirs_exist_ok=True
-    )
-
-
-def _copy_all_template_files(template_path, project_path):
-    """
-    Copy all template files including cookiecutter.json and hooks/ directory.
-
-    Parameters
-    ----------
-    template_path: ~pathlib.Path
-        Path to the template directory.
-    project_path: ~pathlib.Path
-        Path to the baked project directory.
-
-    """
-    copytree(template_path, project_path, dirs_exist_ok=True)
-
-
-def include_license(license_path, project_dirpath):
-    """
-    Include a desired license into the baked project.
-
-    Parameters
-    ----------
-    license_path: ~pathlib.Path
-        Path to the license template.
-    project_dirpath: ~pathlib.Path
-        Path to the baked project directory.
-
-    Notes
-    -----
-    This function is intended to be used during the pre_gen_project.py hook.
-
-    """
-    copyfile(license_path, project_dirpath + "/" + license_path.name)
