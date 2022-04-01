a245f0748c36922f73dc6d90b7309d0204b5466c
Jorge Martínez <28702884+jorgepiloto@users.noreply.github.com>
2022-04-01 19:22:21 +0200

    Fix Python templates metadata (#44)
    * Fix Python templates metadata
    
    * Use private variables in short description key
    
    * Fix license_file key

--- src/ansys/templates/python/pyansys_advanced/{{cookiecutter.__project_name_slug}}/setup_setuptools.py
+++ src/ansys/templates/python/pyansys_advanced/{{cookiecutter.__project_name_slug}}/setup_setuptools.py
@@ -1,22 +1,25 @@
-"""Installation file for ansys-mapdl-core."""
-
 from setuptools import find_namespace_packages, setup
 
 setup(
     name="{{ cookiecutter.__pkg_name }}",
-    packages=find_namespace_packages(where="src", include="ansys*"),
-    package_dir={"": "src"},
-    version="{{ cookiecutter.version }}",
-    description="{{ cookiecutter.short_description }}",
-    long_description=open("README.rst").read(),
-    license="MIT",
+    version="{{ cookiecutter.__version }}",
+    url="{{ cookiecutter.__repository_url }}",
+    author="ANSYS, Inc.",
+    author_email="pyansys.support@ansys.com",
+    maintainer="PyAnsys developers",
+    maintainer_email="pyansys.maintainers@ansys.com",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    url="{{ cookiecutter.repository_url }}",
-    python_requires=">={{ cookiecutter.requires_python }}",
+    license="MIT",
+    license_file="LICENSE",
+    description="{{ cookiecutter.__short_description }}",
+    long_description=open("README.rst").read(),
     install_requires=["importlib-metadata >=4.0"],
+    python_requires=">={{ cookiecutter.__requires_python }}",
+    packages=find_namespace_packages(where="src", include="ansys*"),
+    package_dir={"": "src"},
 )
