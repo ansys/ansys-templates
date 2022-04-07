bf851b453401ca686c7fac0500e2b69da03a590e
Jorge Martinez <jorge.martinezgarrido@ansys.com>
2022-04-07 10:58:59 +0200

    Simplify pyansys-advanced post-hook

--- src/ansys/templates/python/pyansys_advanced/hooks/post_gen_project.py
+++ src/ansys/templates/python/pyansys_advanced/hooks/post_gen_project.py
@@ -9,39 +9,6 @@ ALLOWED_BUILD_SYSTEMS = ["flit", "poetry", "setuptools"]
 """A list of all allowed build systems by the template."""
 
 
-def remove_tool_files(tool_name, basedir):
-    """
-    Remove files matching given glob expression within desired base directory.
-
-    Parameters
-    ----------
-    tool_name: str
-        Name of the tool used as build system.
-    basedir: Path
-        Base directory path.
-
-    """
-    for filepath in basedir.glob(f"**/*_{tool_name}*"):
-        filepath.unlink()
-
-
-def rename_tool_files(tool_name, basedir):
-    """
-    Rename tool filenames within desired base directory.
-
-    Parameters
-    ----------
-    tool_name: str
-        Name of the tool used as build system.
-    basedir: Path
-        Base directory path.
-
-    """
-    for original_filepath in basedir.glob(f"**/*_{tool_name}*"):
-        new_filename = original_filepath.name.replace(f"_{tool_name}", "")
-        original_filepath.rename(Path(original_filepath.parent, new_filename))
-
-
 def main():
     """Entry point of the script."""
     # Get baked project location path
@@ -50,13 +17,10 @@ def main():
     # Get the desired build system
     build_system = "{{ cookiecutter.build_system }}"
 
-    # Remove non-desired build system files
-    for tool in ALLOWED_BUILD_SYSTEMS:
-        if tool != build_system:
-            remove_tool_files(tool, project_path)
-
-    # Rename any files including tool name suffix
-    rename_tool_files(build_system, project_path)
+    # Remove setup.py file if not required
+    if build_system in ["flit", "poetry"]:
+        setup_file = project_path / "setup.py"
+        setup_file.unlink()
 
     # Move all requirements files into a requirements/ directory
     os.mkdir(project_path / "requirements")
