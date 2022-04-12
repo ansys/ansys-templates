f3e83fe339242427f5e83d12b923a3b881caf0af
Babacar Fall <babacar.fall@ansys.com>
2022-04-12 11:12:07 +0200

    Add Flask template

--- src/ansys/templates/paths.py
+++ src/ansys/templates/paths.py
@@ -29,8 +29,8 @@ PYTHON_TEMPLATES_PYACE_PATH = PYTHON_TEMPLATES_PATH / "pyace_pkg"
 PYTHON_TEMPLATES_PYACE_GRPC_PATH = PYTHON_TEMPLATES_PATH / "pyace_grpc"
 """Path to the gRPC based Python Project template."""
 
-# PYTHON_TEMPLATES_PYACE_FLASK_PATH = PYTHON_TEMPLATES_PATH / "pyace_flask"
-# """Path to the Flask based Python Project template."""
+PYTHON_TEMPLATES_PYACE_FLASK_PATH = PYTHON_TEMPLATES_PATH / "pyace_flask"
+"""Path to the Flask based Python Project template."""
 
 # PYTHON_TEMPLATES_PYACE_FAST_PATH = PYTHON_TEMPLATES_PATH / "pyace_fastapi"
 # """Path to the FastAPI based Python Project template."""
@@ -41,7 +41,7 @@ TEMPLATE_PATH_FINDER = {
     "pyansys_advanced": PYTHON_TEMPLATES_PYANSYS_ADVANCED_PATH,
     "pyace": PYTHON_TEMPLATES_PYACE_PATH,
     "pyace-grpc": PYTHON_TEMPLATES_PYACE_GRPC_PATH,
-    # "pyace-flask": PYTHON_TEMPLATES_PYACE_FLASK_PATH,
+    "pyace-flask": PYTHON_TEMPLATES_PYACE_FLASK_PATH,
     # "pyace-fastapi": PYTHON_TEMPLATES_PYACE_FAST_PATH,
 }
 """A dictionary relating templates names with their paths."""
