ebff4c9b956a1e0e196136029f77ca619390e2ad
jorgepiloto <jorgepiloto@users.noreply.github.com>
2022-06-29 07:44:10 +0000

    Update demo/pyansys to output generated at 00b2cd7

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,6 +1,10 @@
 """Sphinx documentation configuration file."""
 from datetime import datetime
-from ansys_sphinx_theme import pyansys_logo_black as logo# Project informationproject = "ansys-product-library"copyright = f"(c) {datetime.now().year} ANSYS, Inc. All rights reserved"
+from ansys_sphinx_theme import pyansys_logo_black as logo
+
+# Project information
+project = "ansys-product-library"
+copyright = f"(c) {datetime.now().year} ANSYS, Inc. All rights reserved"
 author = "ANSYS, Inc."
 release = version = "0.1.dev0"
 
@@ -14,7 +18,9 @@ html_theme_options = {
     "github_url": "https://github.com/pyansys/pyproduct-library",
     "show_prev_next": False,
     "show_breadcrumbs": True,
-    "additional_breadcrumbs": [("PyAnsys", "https://docs.pyansys.com/"),],
+    "additional_breadcrumbs": [
+        ("PyAnsys", "https://docs.pyansys.com/"),
+    ],
 }
 
 # Sphinx extensions
