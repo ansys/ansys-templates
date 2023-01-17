80d6c05aa7bd988edef02d46175e30e3189e56bc
Ismael AZEHAF <ismael.azehaf@ansys.com>
2023-01-17 10:31:19 +0100

    Linting

--- tests/tests_templates/test_python_templates.py
+++ tests/tests_templates/test_python_templates.py
@@ -366,4 +366,3 @@ def test_template_python(tmp_path, build_system, template):
 
     # Check that all common files are included in baked project
     assert_project_structure(EXPECTED_STRUCTURE, project_path)
-
