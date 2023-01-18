9f34a449ef425d70ac28322b99bb7b9f6f241231
Ismael AZEHAF <ismael.azehaf@ansys.com>
2023-01-18 12:25:55 +0100

    Parametrize no-ui option

--- src/ansys/templates/python/solution/{{cookiecutter.__project_name_slug}}/src/ansys/solutions/{{cookiecutter.__solution_name_slug}}/main.py
+++ src/ansys/templates/python/solution/{{cookiecutter.__project_name_slug}}/src/ansys/solutions/{{cookiecutter.__solution_name_slug}}/main.py
@@ -5,19 +5,19 @@
 from ansys.saf.glow.runtime import glow_main
 
 from ansys.solutions.{{ cookiecutter.__solution_name_slug }}.solution import definition
-{%- if cookiecutter.no_ui == "False" %}
+{% if cookiecutter.no_ui == "False" %}
 from ansys.solutions.{{ cookiecutter.__solution_name_slug }}.ui import app
-{%- endif %}
+{% endif %}
 
 
 def main():
     """Entry point."""
-    
-    {%- if cookiecutter.no_ui == "False" %}
+
+    {% if cookiecutter.no_ui == "False" %}
     glow_main("{{ cookiecutter.__solution_name_slug }}", definition, app)
-    {%- else -%}
+    {% else %}
     glow_main("{{ cookiecutter.__solution_name_slug }}", definition)
-    {%- endif %}
+    {% endif %}
 
 
 if __name__ == "__main__":
