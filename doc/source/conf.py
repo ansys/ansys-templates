4046bfb536406da9fac2ad1d7723a1794ab9117e
iazehaf <iazehaf@users.noreply.github.com>
2023-03-20 10:53:09 +0000

    Update demo/pybasic to output generated at 5ce3b6e

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,18 +1,12 @@
 """Sphinx documentation configuration file."""
 from datetime import datetime
-import os
-
-from ansys_sphinx_theme import get_version_match
 from ansys_sphinx_theme import pyansys_logo_black as logo
-from pybasic import __version__
-
 
 # Project information
 project = "pybasic"
 copyright = f"(c) {datetime.now().year} ANSYS, Inc. All rights reserved"
 author = "ANSYS, Inc."
-release = version = __version__
-cname = os.getenv("DOCUMENTATION_CNAME")
+release = version = "0.1.dev0"
 
 # Select desired logo, theme, and declare the html title
 html_logo = logo
@@ -27,11 +21,6 @@ html_theme_options = {
     "additional_breadcrumbs": [
         ("PyAnsys", "https://docs.pyansys.com/"),
     ],
-    "switcher": {
-        "json_url": f"https://{cname}/versions.json",
-        "version_match": get_version_match(__version__),
-    },
-    "check_switcher": False,
 }
 
 # Sphinx extensions
