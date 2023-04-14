051b5437db8c1bcf032bbec6b46862a98dd7f1af
ansRMorin <rosalie.morin@ansys.com>
2023-04-13 20:20:18 -0400

    add content to problem_setup_step

--- src/ansys/templates/python/osl_solution/{{cookiecutter.__project_name_slug}}/src/ansys/solutions/{{cookiecutter.__solution_name_slug}}/solution/problem_setup_step.py
+++ src/ansys/templates/python/osl_solution/{{cookiecutter.__project_name_slug}}/src/ansys/solutions/{{cookiecutter.__solution_name_slug}}/solution/problem_setup_step.py
@@ -2,18 +2,47 @@
 
 """Backend of the problem setup step."""
 
+from pathlib import Path
 
-from ansys.saf.glow.solution import StepModel, StepSpec, transaction
-
+from ansys.saf.glow.solution import StepModel, StepSpec, transaction, FileReference
+from ansys.solutions.optislang.parser.placeholder import ProjectProperties
 
 class ProblemSetupStep(StepModel):
     """Step definition of the first step."""
 
-    first_arg: float = 0
-    second_arg: float = 0
-    result: float = 0
+    placeholder_values = {}
+    placeholder_definitions = {}
 
-    @transaction(self=StepSpec(upload=["result"], download=["first_arg", "second_arg"]))
-    def calculate(self) -> None:
-        """Method to compute the sum of two numbers."""
-        self.result = self.first_arg + self.second_arg
+    # File storage
+    project_file: FileReference = FileReference("Problem_Setup/hook_optimization.opf") # WARNING! Must be defined for specific use case
+    properties_file: FileReference = FileReference("Problem_Setup/hook_optimization.json") # WARNING! Must be defined for specific use case
+
+    
+    @transaction(self=StepSpec(download=["properties_file"], upload=["placeholder_values", "placeholder_definitions"]))
+    def get_default_placeholder_values(self):
+        """Get placeholder values and definitions using the ProjectProperties class."""
+
+        pp = ProjectProperties()
+        pp.read_file(self.properties_file.path)
+        placeholders = pp.get_properties()['placeholders']
+        self.placeholder_values = placeholders.get('placeholder_values')
+        self.placeholder_definitions = placeholders.get('placeholder_definitions')
+        
+
+    @transaction(self=StepSpec(upload=["project_file"]))
+    def upload_project_file_to_project_directory(self) -> None:
+        """Upload OptiSLang project file to project directory."""
+
+        original_project_file = (
+            Path(__file__).parent.absolute().parent / "model" / "assets" / "hook_optimization.opf"  # WARNING! Must be defined for specific use case
+        )
+        self.project_file.write_bytes(original_project_file.read_bytes())
+    
+    @transaction(self=StepSpec(upload=["properties_file"]))
+    def upload_properties_file_to_project_directory(self) -> None:
+        """Upload OptiSLang properties to project directory."""
+
+        original_properties_file = (
+            Path(__file__).parent.absolute().parent / "model" / "assets" / "hook_optimization.json" # WARNING! Must be defined for specific use case
+        )
+        self.properties_file.write_bytes(original_properties_file.read_bytes())
--- src/ansys/templates/python/osl_solution/{{cookiecutter.__project_name_slug}}/src/ansys/solutions/{{cookiecutter.__solution_name_slug}}/ui/problem_setup_page.py
+++ src/ansys/templates/python/osl_solution/{{cookiecutter.__project_name_slug}}/src/ansys/solutions/{{cookiecutter.__solution_name_slug}}/ui/problem_setup_page.py
@@ -2,47 +2,120 @@
 
 """Frontend of the first step."""
 
+import dash_bootstrap_components as dbc
 
 from ansys.saf.glow.client.dashclient import DashClient
 from dash_extensions.enrich import Input, Output, State, callback, dcc, html
+from ansys.solutions.dash_components.table import InputRow
+from ansys.solutions.optislang.frontend_components.placeholder_table import PlaceholderTable
+
 
 from ansys.solutions.{{ cookiecutter.__solution_name_slug }}.solution.definition import {{cookiecutter.__solution_definition_name}}
 from ansys.solutions.{{ cookiecutter.__solution_name_slug }}.solution.problem_setup_step import ProblemSetupStep
 
 
 def layout(problem_setup_step: ProblemSetupStep):
-    """Layout of the first step UI."""
-    return html.Div(
+    """Layout of the problem setup step UI."""
+
+    # Upload placeholders and assets to project directory
+    if problem_setup_step.placeholder_values == {}:
+        problem_setup_step.upload_project_file_to_project_directory()
+        problem_setup_step.upload_properties_file_to_project_directory()
+        problem_setup_step.get_default_placeholder_values()
+    
+    placeholder_table = PlaceholderTable(problem_setup_step.placeholder_values, problem_setup_step.placeholder_definitions)
+
+    # Placeholder card for displaying parameters defined in the <project_name>.json
+    placeholder_card = dbc.Card(
         [
-            dcc.Markdown("""#### First step""", className="display-3"),
-            dcc.Markdown("""###### Compute the sum of two numbers.""", className="display-3"),
+            dbc.CardBody(
+                [
+                dbc.Accordion(
+                    [   
+                        dbc.AccordionItem(
+                            [
+                                html.Div(placeholder_table.create()),
+                            ],
+                            
+                            title="Placeholders",
+                            item_id="parameter-placeholders",
+                        )
+                    ]
+                )
+            ]
+            )
+        ]            
+    )
+    return html.Div(
+        [   
+            html.H1("Optislang Project Name", className="display-3", style={"font-size": "35px"}),
+            html.P(
+                "An optiSLang web application for the optimization of a steel hook.", # Example of DESCRIPTION_TEXT from load_wizard.py
+                className="lead",
+                style={"font-size": "20px"},
+            ),
             html.Hr(className="my-2"),
             html.Br(),
-            html.Div(["First Argument: ", dcc.Input(id="first-arg", value=step.first_arg, type="number")]),
-            html.Div(["Second Argument: ", dcc.Input(id="second-arg", value=step.second_arg, type="number")]),
-            html.Button("Calculate", id="calculate", n_clicks=0),
-            html.P(id="result", children=f"Result:{step.result}"),
+            dbc.Row(
+                [
+                    # dbc.Row(image_card), 
+                    dbc.Row(placeholder_card),
+                ],
+                style={"padding": "20px"},
+            ),
+            dbc.Row(
+                [
+                    dbc.Accordion(
+                        [   
+                            dbc.AccordionItem(
+                                [
+                                    InputRow(
+                                        "input",
+                                        "project_name_input",
+                                        "Project settings",
+                                        row_default_value=None,
+                                        row_description="Enter a project name.",
+                                        label_width=2,
+                                        value_width=4,
+                                        unit_width=1,
+                                        description_width=4,
+                                    ).get(),
+                                    html.Br(),
+                                    InputRow(
+                                        "button",
+                                        "start_analysis",
+                                        "Start analysis",
+                                        disabled=False,
+                                        label_width=2,
+                                        value_width=4,
+                                        unit_width=1,
+                                        description_width=4,
+                                        class_name="button",
+                                    ).get(),
+                                    dcc.Loading(
+                                        id="optislang_wait_spinner",
+                                        type="circle",
+                                        fullscreen=True,
+                                        color="#ffb71b",
+                                        style={
+                                            "background-color": "rgba(55, 58, 54, 0.1)",
+                                        },
+                                        children=html.Div(id="wait_optislang_process"),
+                                    ),
+                                    dcc.Interval(
+                                        id="solve-interval-component",
+                                        interval=1 * 6000,  # in milliseconds
+                                        n_intervals=0,
+                                    ),
+                                ],
+                                title="Start Analysis",
+                                item_id="start-analysis-accordion",
+                            ),
+                        ]
+                    )
+                ]
+            ),
         ]
     )
 
 
-# This is an example callback which stores entered data and executes a method.
-# This is the only place where user entered data is persisted in this Dash app.
-# Notice that the project is referenced by a call to get_project and
-# the url is a State argument to the callback
-@callback(
-    Output("result", "children"),
-    Input("calculate", "n_clicks"),
-    State("first-arg", "value"),
-    State("second-arg", "value"),
-    State("url", "pathname"),
-    prevent_initial_call=True,
-)
-def calculate(n_clicks, first_arg, second_arg, pathname):
-    """Callback function to trigger the computation."""
-    project = DashClient[{{cookiecutter.__solution_definition_name}}].get_project(pathname)
-    step = project.steps.first_step
-    step.first_arg = first_arg
-    step.second_arg = second_arg
-    step.calculate()
-    return f"Result:{step.result}"
