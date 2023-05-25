e3b4da8018175ca4bfc948a1141354b63fc4a3f0
Rosalie Morin <rosalie.morin@ansys.com>
2023-05-25 15:04:52 -0400

    remove solution created for testing

--- opf-soln/src/ansys/solutions/opf_soln/model/README.md
+++
@@ -1,1 +0,0 @@
-# Add modules related to the simulation models.
--- opf-soln/tests/integration/test_integration_dummy.py
+++
@@ -1,7 +0,0 @@
-# ©2022, ANSYS Inc. Unauthorized use, distribution or duplication is prohibited.
-
-import ansys.solutions.opf_soln
-
-
-def test_expected_to_pass():
-    assert True
