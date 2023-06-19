e39491fdd7c24167c9e7ac8c081ac6ce8482fab6
Ismael AZEHAF <ismael.azehaf@ansys.com>
2023-06-19 10:22:20 +0200

    Update osl template.

--- my-osl-solution/src/ansys/solutions/my_osl_solution/model/scripts/README.md
+++
@@ -1,1 +0,0 @@
-# Add scripts related to the simulation models.
--- my-osl-solution/tests/integration/test_integration_dummy.py
+++
@@ -1,7 +0,0 @@
-# ©2022, ANSYS Inc. Unauthorized use, distribution or duplication is prohibited.
-
-import ansys.solutions.my_osl_solution
-
-
-def test_expected_to_pass():
-    assert True
