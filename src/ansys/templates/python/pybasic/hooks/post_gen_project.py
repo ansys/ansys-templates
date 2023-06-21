0af79cf4caa4baa5a22a57934fb271443510a478
Jorge Martinez Garrido <jorge.martinezgarrido@ansys.com>
2023-06-21 16:23:13 +0200

    FIX: reset file

--- src/ansys/templates/python/pybasic/hooks/post_gen_project.py
+++ src/ansys/templates/python/pybasic/hooks/post_gen_project.py
@@ -1,15 +1,37 @@
 from ansys.templates.utils import keep_files
 
+
 DESIRED_STRUCTURE = [
-    "README.md",
+    ".coveragerc",
+    "CHANGELOG.md",
+    "CODE_OF_CONDUCT.md",
+    "CONTRIBUTING.md",
+    "doc/Makefile",
+    "doc/make.bat",
+    "doc/.vale.ini",
+    "doc/styles/.gitignore",
+    "doc/styles/Vocab/ANSYS/accept.txt",
+    "doc/styles/Vocab/ANSYS/reject.txt",
+    "doc/source/conf.py",
+    "doc/source/index.rst",
+    "doc/source/_static/README.md",
+    "doc/source/_templates/README.md",
+    "examples/README.md",
+    ".flake8",
+    ".gitattributes",
+    ".gitignore",
+    "LICENSE",
     "pyproject.toml",
+    "README.rst",
+    "requirements_build.txt",
+    "requirements_doc.txt",
+    "requirements_tests.txt",
     "setup.py",
-    "src/ansys/api/{{ cookiecutter.__project_name_slug }}/__init__.py",
-    "src/ansys/api/{{ cookiecutter.__project_name_slug }}/VERSION",
-    "src/ansys/api/{{ cookiecutter.__project_name_slug }}/py.typed",
-    "src/ansys/api/{{ cookiecutter.__project_name_slug }}/v0",
+    "src/{{ cookiecutter.__project_name_slug }}/__init__.py",
+    "tests/test_metadata.py",
 ]
 """A list holding all desired files to be included in the project."""
+
 
 def main():
     """Entry point of the script."""
