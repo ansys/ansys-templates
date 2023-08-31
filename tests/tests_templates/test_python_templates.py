c10c3c7a309a1634790cedf980f3a4365aa1e83f
ansRMorin <rosalie.morin@ansys.com>
2023-08-31 16:25:47 -0400

    update test

--- tests/tests_templates/test_python_templates.py
+++ tests/tests_templates/test_python_templates.py
@@ -375,6 +375,7 @@ OSL_SOLUTION_STRUCTURE = [
     f"src/ansys/solutions/{OSL_SOLUTION_VARS['__solution_name_slug']}/ui/components/actor_information_table.py",
     f"src/ansys/solutions/{OSL_SOLUTION_VARS['__solution_name_slug']}/ui/components/actor_logs_table.py",
     f"src/ansys/solutions/{OSL_SOLUTION_VARS['__solution_name_slug']}/ui/components/actor_statistics_table.py",
+    f"src/ansys/solutions/{OSL_SOLUTION_VARS['__solution_name_slug']}/ui/components/button_group.py",
     f"src/ansys/solutions/{OSL_SOLUTION_VARS['__solution_name_slug']}/ui/components/commands.py",
     f"src/ansys/solutions/{OSL_SOLUTION_VARS['__solution_name_slug']}/ui/components/design_table.py",
     f"src/ansys/solutions/{OSL_SOLUTION_VARS['__solution_name_slug']}/ui/components/logs_table.py",
