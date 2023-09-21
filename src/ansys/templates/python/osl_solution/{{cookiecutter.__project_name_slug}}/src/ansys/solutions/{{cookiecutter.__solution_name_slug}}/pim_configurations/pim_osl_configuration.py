b1430034e8989a26630e09b052155dab7e36e168
Pierre Blanc <pierre.blanc@ansys.com>
2023-09-21 11:20:31 +0200

    Fix uvicorn location in optislang template
    uvicorn.exe is not available in the optislang environment, so let's
    use python to start uvicorn  via python -m uvicorn

--- src/ansys/templates/python/osl_solution/{{cookiecutter.__project_name_slug}}/src/ansys/solutions/{{cookiecutter.__solution_name_slug}}/pim_configurations/pim_osl_configuration.py
+++ src/ansys/templates/python/osl_solution/{{cookiecutter.__project_name_slug}}/src/ansys/solutions/{{cookiecutter.__solution_name_slug}}/pim_configurations/pim_osl_configuration.py
@@ -11,12 +11,11 @@ from ansys.saf.glow.desktop.orchestration.pim_process import PimProductConfigura
 
 class OptislangConfigBuilder(PimProductConfigurationBuilder):
     def get_configurations(self) -> List[PimProductConfiguration]:
-        uvicorn_exe = Path(sys.executable).parent / "uvicorn"
         return [
             PimProductConfiguration(
                 "optislang",
-                str(uvicorn_exe),
-                arguments=["ansys.solutions.{{ cookiecutter.__solution_name_slug }}.pim_configurations.osl_wrapper:app", "--port", "${AENEID_PORT_HTTP}"],
+                str(sys.executable),
+                arguments=["m", "ansys.solutions.{{ cookiecutter.__solution_name_slug }}.pim_configurations.osl_wrapper:app", "--port", "${AENEID_PORT_HTTP}"],
                 service_name="http",
                 service_type="http",
             )
