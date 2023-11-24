ac93bbbc7a0aecc57833c885a80ce724c41b8fad
ravi-krsharma <108462238+ravi-krsharma@users.noreply.github.com>
2023-11-24 17:01:50 +0530

    Add callback for update div in visualization view

--- src/ansys/templates/python/osl_solution/{{cookiecutter.__project_name_slug}}/src/ansys/solutions/{{cookiecutter.__solution_name_slug}}/ui/views/visualization_view.py
+++ src/ansys/templates/python/osl_solution/{{cookiecutter.__project_name_slug}}/src/ansys/solutions/{{cookiecutter.__solution_name_slug}}/ui/views/visualization_view.py
@@ -18,11 +18,74 @@ def layout(problem_setup_step: ProblemSetupStep, monitoring_step: MonitoringStep
 
     return html.Div(
         [
-            optislang_dash_lib.VisualizationComponent(
-                id="visualization-component",
-                project_state=full_project_status_info,
-                system_id=monitoring_step.selected_actor_from_treeview,
-                state_idx=0,
+            html.Div(
+                id="visualization-component-div1"
             ),
+            html.Div(
+                id="visualization-component-div2"
+            ),
+            dcc.Interval(
+                id="visualization_auto_update",
+                interval=monitoring_step.auto_update_frequency,  # in milliseconds
+                n_intervals=0,
+                disabled=False if monitoring_step.auto_update_activated else True,
+            )
+            
         ]
     )
+
+
+
+    @callback(
+        Output("visualization-component-div1", "children"),
+        Output("visualization-component-div2", "children"),
+        Output("visualization-component-div1", "style"),
+        Output("visualization-component-div2", "style"),
+        Output("visualization-store", "value"),
+        Input("visualization_auto_update", "n_intervals"),
+        State("url", "pathname"),
+    )
+    def update_view(n_intervals, count, pathname):
+        """Update design table."""
+    
+        # Get project
+        project = DashClient[{{ cookiecutter.__solution_definition_name }}].get_project(pathname)
+        # Get problem setup step
+        problem_setup_step = project.steps.problem_setup_step
+        # Get monitoring step
+        monitoring_step = project.steps.monitoring_step
+    
+        if monitoring_step.auto_update_activated:
+            print("Interval call in visualization view")
+            # Get project data
+            full_project_status_info = json.loads(problem_setup_step.full_project_status_info_file.read_text())
+            
+            visualization_status_view_1_child = None
+            visualization_status_view_1_style = None
+            visualization_status_view_2_child = None
+            visualization_status_view_2_style = None
+            
+            if n_intervals % 2==0: #(even)
+                visualization_status_view_1_child = optislang_dash_lib.VisualizationComponent(
+                            id="visualization-component1",
+                            project_state=full_project_status_info,
+                            system_id=monitoring_step.selected_actor_from_treeview,
+                            state_idx=0,
+                        )
+                visualization_status_view_1_style = {'display': 'block'}
+                visualization_status_view_2_style = {'display': 'none'}
+            else:
+                visualization_status_view_2_child = optislang_dash_lib.VisualizationComponent(
+                            id="visualization-component1",
+                            project_state=full_project_status_info,
+                            system_id=monitoring_step.selected_actor_from_treeview,
+                            state_idx=0,
+                        )
+                visualization_status_view_2_style = {'display': 'block'}
+                visualization_status_view_1_style = {'display': 'none'}
+            
+    
+            return visualization_status_view_1_child, visualization_status_view_1_style, visualization_status_view_2_child, visualization_status_view_2_style
+    
+        else:
+            raise PreventUpdate
