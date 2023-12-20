560a79ee83b1d44a98e3c48a831adfc646091621
Revathyvenugopal162 <104772255+Revathyvenugopal162@users.noreply.github.com>
2023-12-20 10:43:07 +0000

    Update demo/pyace-flask to output generated at 1abbc11

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     description="A my_company Python project for my_company library",
     long_description=open("README.rst").read(),
     install_requires=["importlib-metadata >=4.0"],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     packages=find_namespace_packages(where="src"),
     package_dir={"": "src"},
 )
