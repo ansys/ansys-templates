d14b7cddf328a788a1c1abd56bfff3054236fbc7
iazehaf <107680473+iazehaf@users.noreply.github.com>
2025-02-18 08:10:27 +0000

    Update demo/solution to output generated at 2dad506

--- setup_environment.py
+++ setup_environment.py
@@ -345,6 +345,43 @@ def check_inputs(args: object) -> None:
                     "how to enable this at https://pip.pypa.io/warnings/enable-long-paths"
                 )
 
+    private_sources = get_private_sources("pyproject.toml")
+    for source in private_sources:
+        if source["name"].lower() == "pypi":
+            continue
+        token_environment_variable = source["name"].upper().replace("-", "_") + "_TOKEN"
+        token = os.getenv(token_environment_variable, None)
+        if token:
+            url_scheme, relative_url = remove_scheme_from_url(source["url"])
+            command = [
+                sys.executable,
+                "-m",
+                "pip",
+                "install",
+                "ansys-saf-glow-engine==x",
+                "-i",
+                f"{url_scheme}://{token}@{relative_url}",
+            ]
+
+            process = subprocess.run(
+                command,
+                check=False,
+                shell=DEPENDENCY_MANAGER_PATHS[sys.platform]["shell"],
+                capture_output=True,
+                text=True,
+            )
+            if process.returncode != 0:
+                if "401 Error, Credentials not correct" in str(process.stderr):
+                    raise Exception(
+                        f"Token for {source['name']} is incorrect. "
+                        "Please set the correct token in the environment variable {token_environment_variable}."
+                    )
+        else:
+            raise Exception(
+                f"Token for {source['name']} is missing. "
+                "Please set the environment variable {token_environment_variable} with the token."
+            )
+
 
 def modify_toml_file_in_case_of_wheel_files(args: object) -> None:
     """Modify the toml file in case of wheel files."""
