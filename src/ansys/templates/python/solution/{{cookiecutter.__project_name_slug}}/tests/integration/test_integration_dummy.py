7211f2dbc7a5647e6b02eae5bc5284cdce368743
ansjgculebras <javier.garcia@ansys.com>
2025-04-16 10:18:52 +0200

    Remove solution template folder

--- src/ansys/templates/python/solution/{{cookiecutter.__project_name_slug}}/tests/integration/test_integration_dummy.py
+++
@@ -1,7 +0,0 @@
-# ©2023, ANSYS Inc. Unauthorized use, distribution or duplication is prohibited.
-
-import ansys.solutions.{{ cookiecutter.__solution_name_slug }}
-
-
-def test_expected_to_pass():
-    assert True
