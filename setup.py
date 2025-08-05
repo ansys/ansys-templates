ba518907eaf4dcc3fc132d3389c81074752f0551
klmcadams <58492561+klmcadams@users.noreply.github.com>
2025-08-05 14:50:34 +0000

    Update demo/pyansys to output generated at 3ad74c2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     description="A python wrapper for ansys product library",
     long_description=open("README.rst").read(),
     install_requires=["importlib-metadata >=4.0"],
-    python_requires=">=3.9",
+    python_requires=">=3.12",
     packages=find_namespace_packages(where="src", include="ansys*"),
     package_dir={"": "src"},
 )
